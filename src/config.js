367cb847609de245b8d0f8de76adb74fb847c35c
rhamonantunes <66077933+rhamonantunes@users.noreply.github.com>
2025-12-01 18:35:20 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://webhook-app-backend-webhook-app.apps.rjoseant.vmware.tamlab.rdu2.redhat.com";
+export const API_URL = "https://webhook-app-backend-webhook-app.apps.cluster-hfz8n.hfz8n.sandbox2230.opentlc.com";
